a69c43a36bc64d44d5a878288fa4d890d4254da0
ArthurBArre <arthur_barre@outlook.fr>
2019-09-30 18:24:55 +0200

    added static folder and prepared spa

--- server/index.js
+++ server/index.js
@@ -12,4 +12,13 @@ const posts = require('./routes/api/posts')
 app.use('/api/posts', posts);
 const port = process.env.PORT || 3000;
 
+//handle prod
+if (process.env.NODE_ENV === 'production') {
+  //static folder
+  app.use(express.static(__dirname + '/public/'))
+
+  //handle spa
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
+
 app.listen(port, () => console.log(`server run on ${port}`))
